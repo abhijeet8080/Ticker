51a6a617e373ae291101e5b7df3349a0ce70fa67
unknown <kadamabhi1881@gmail.com>
2023-07-31 10:11:30 +0530

    Add some new background images

--- index.js
+++ index.js
@@ -5,7 +5,16 @@ const changeBg = ()=>{
     "url(2.jpg)",
     "url(3.jpg)",
     "url(4.jpg)",
-    "url(5.jpg)"
+    "url(5.jpg)",
+    "url(6.jpg)",
+    "url(7.jpg)",
+    "url(8.jpg)",
+    "url(9.jpg)",
+    "url(10.jpg)",
+    "url(11.jpg)",
+    "url(12.jpg)",
+    "url(13.jpg)",
+    "url(14.jpg)",
   ];
 const bodyImg = document.querySelector("body");
 const bg = images[Math.floor(Math.random()*images.length)];
